ad7a37c02faf35b2f72433ae7a778035d7335887
jurplel <jeep70cp@gmail.com>
2024-09-04 22:32:50 +0000

    Build: (90affc8) update for lec2

--- hw/getownedlab/doc/src-files.js
+++ hw/getownedlab/doc/src-files.js
@@ -1,4 +1,4 @@
-var srcIndex = JSON.parse('{\
-"getownedlab":["",[["move_semantics",[],["mod.rs"]],["slices",[],["mod.rs","slices1.rs"]],["strings",[],["mod.rs"]]],["lib.rs"]]\
-}');
+var srcIndex = new Map(JSON.parse('[\
+["getownedlab",["",[["move_semantics",[],["mod.rs"]],["slices",[],["mod.rs","slices1.rs"]],["strings",[],["mod.rs"]]],["lib.rs"]]]\
+]'));
 createSrcSidebar();
