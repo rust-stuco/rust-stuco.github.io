4474e02ccd367cd844dd3f9bd24f578a280ace31
Jessica Ruan <jessica.ruan@outlook.com>
2025-01-03 12:02:13 -0800

    migrate f24 to old

---
+++ old/f24/hw/getownedlab/doc/src-files.js
@@ -0,0 +1,4 @@
+var srcIndex = new Map(JSON.parse('[\
+["getownedlab",["",[["move_semantics",[],["mod.rs"]],["slices",[],["mod.rs","slices1.rs"]],["strings",[],["mod.rs"]]],["lib.rs"]]]\
+]'));
+createSrcSidebar();
---
+++ old/f24/hw/primerlab/doc/src-files.js
@@ -0,0 +1,4 @@
+var srcIndex = new Map(JSON.parse('[\
+["primerlab",["",[["exercises",[],["fixme1.rs","mod.rs"]]],["functions.rs","lib.rs"]]]\
+]'));
+createSrcSidebar();
