32b2c7baa6ed0bf8a39aebb02d2b242d9759e24a
jespiron <jessica.ruan@outlook.com>
2025-01-04 16:09:30 +0000

    Build: (4474e02) migrate f24 to old

---
+++ old/f24/hw/getownedlab/doc/search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = new Map(JSON.parse('[\
+["getownedlab",{"doc":"98-008: Intro to the Rust Programming Language","t":"CCCC","n":["move_semantics","slices","strings","slices1"],"q":[[0,"getownedlab"],[3,"getownedlab::slices"]],"d":["All examples taken from rustlings exercises and practice.rs","All examples taken from practice.rs","All examples taken from rustlings exercises",""],"i":[0,0,0,0],"f":[0,0,0,0],"c":[],"p":[],"b":[]}]\
+]'));
+if (typeof exports !== 'undefined') exports.searchIndex = searchIndex;
+else if (window.initSearch) window.initSearch(searchIndex);
