e75abb5806c37588f5d6979a073398adafb7e320
connortsui20 <87130162+connortsui20@users.noreply.github.com>
2025-01-14 02:49:48 +0000

    Build: (811c868) update multilab handout and writeup

--- hw/multilab/doc/src-files.js
+++ hw/multilab/doc/src-files.js
@@ -1,4 +1,3 @@
-var srcIndex = JSON.parse('{\
-"multilab":["",[],["lib.rs","multimap.rs","multiset.rs"]]\
-}');
+var srcIndex = new Map(JSON.parse('[["multilab",["",[],["lib.rs","multimap.rs","multiset.rs"]]]]'));
 createSrcSidebar();
+//{"start":36,"fragment_lengths":[59]}
