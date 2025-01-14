0ecac7c53c688324cc169960b618a863927142c0
Connor Tsui <connor.tsui20@gmail.com>
2025-01-13 21:40:27 -0500

    update getownedlab handout and writeup

--- hw/getownedlab/doc/search-index.js
+++ hw/getownedlab/doc/search-index.js
@@ -1,5 +1,4 @@
-var searchIndex = new Map(JSON.parse('[\
-["getownedlab",{"doc":"98-008: Intro to the Rust Programming Language","t":"CCCC","n":["move_semantics","slices","strings","slices1"],"q":[[0,"getownedlab"],[3,"getownedlab::slices"]],"d":["All examples taken from rustlings exercises and practice.rs","All examples taken from practice.rs","All examples taken from rustlings exercises",""],"i":[0,0,0,0],"f":[0,0,0,0],"c":[],"p":[],"b":[]}]\
-]'));
+var searchIndex = new Map(JSON.parse('[["getownedlab",{"t":"CCCC","n":["move_semantics","slices","strings","slices1"],"q":[[0,"getownedlab"],[3,"getownedlab::slices"]],"i":"````","f":"````","D":"h","p":[],"r":[],"b":[],"c":"OjAAAAAAAAA=","e":"OjAAAAEAAAAAAAAAEAAAAAQA","P":[]}]]'));
 if (typeof exports !== 'undefined') exports.searchIndex = searchIndex;
 else if (window.initSearch) window.initSearch(searchIndex);
+//{"start":39,"fragment_lengths":[238]}
--- hw/getownedlab/doc/src-files.js
+++ hw/getownedlab/doc/src-files.js
@@ -1,4 +1,3 @@
-var srcIndex = new Map(JSON.parse('[\
-["getownedlab",["",[["move_semantics",[],["mod.rs"]],["slices",[],["mod.rs","slices1.rs"]],["strings",[],["mod.rs"]]],["lib.rs"]]]\
-]'));
+var srcIndex = new Map(JSON.parse('[["getownedlab",["",[["move_semantics",[],["mod.rs"]],["slices",[],["mod.rs","slices1.rs"]],["strings",[],["mod.rs"]]],["lib.rs"]]]]'));
 createSrcSidebar();
+//{"start":36,"fragment_lengths":[130]}
