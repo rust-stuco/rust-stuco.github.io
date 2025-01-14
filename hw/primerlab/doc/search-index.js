164e2ed573b4b8e3c2470946f200300cc04a6249
connortsui20 <87130162+connortsui20@users.noreply.github.com>
2025-01-14 02:35:05 +0000

    Build: (7e41bac) update primerlab handout and writeup

--- hw/primerlab/doc/search-index.js
+++ hw/primerlab/doc/search-index.js
@@ -1,5 +1,4 @@
-var searchIndex = new Map(JSON.parse('[\
-["primerlab",{"doc":"98-008: Intro to the Rust Programming Language","t":"CCCHHHHH","n":["exercises","functions","fixme1","add","fib","gcd","is_prime","nth_prime"],"q":[[0,"primerlab"],[2,"primerlab::exercises"],[3,"primerlab::functions"]],"d":["All examples taken from rustlings exercises","","","Solutions and some test cases adapted from 15-112.","Returns the nth fibonacci number.","Returns the Greatest Common Divisor (gcd) of two numbers x …","Given a number n, return true if it is a prime number, and …","Given a number n, return the nth prime. Refer to the test …"],"i":[0,0,0,0,0,0,0,0],"f":[0,0,0,[[1,1],1],[1,1],[[1,1],1],[1,2],[1,1]],"c":[],"p":[[1,"usize"],[1,"bool"]],"b":[]}]\
-]'));
+var searchIndex = new Map(JSON.parse('[["primerlab",{"t":"CCCHHHHH","n":["exercises","functions","fixme1","add","fib","gcd","is_prime","nth_prime"],"q":[[0,"primerlab"],[2,"primerlab::exercises"],[3,"primerlab::functions"]],"i":"````````","f":"```{{bb}b}{bb}1{bd}1","D":"n","p":[[1,"usize"],[1,"bool"]],"r":[],"b":[],"c":"OjAAAAAAAAA=","e":"OjAAAAEAAAAAAAEAEAAAAAIAAwA=","P":[]}]]'));
 if (typeof exports !== 'undefined') exports.searchIndex = searchIndex;
 else if (window.initSearch) window.initSearch(searchIndex);
+//{"start":39,"fragment_lengths":[340]}
