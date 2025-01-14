7e41bac526c88fab013d9272757a664cd70e38a1
Connor Tsui <connor.tsui20@gmail.com>
2025-01-13 21:34:17 -0500

    update primerlab handout and writeup

--- hw/primerlab/doc/crates.js
+++ hw/primerlab/doc/crates.js
@@ -1,1 +1,2 @@
 window.ALL_CRATES = ["primerlab"];
+//{"start":21,"fragment_lengths":[11]}
--- hw/primerlab/doc/src-files.js
+++ hw/primerlab/doc/src-files.js
@@ -1,4 +1,3 @@
-var srcIndex = new Map(JSON.parse('[\
-["primerlab",["",[["exercises",[],["fixme1.rs","mod.rs"]]],["functions.rs","lib.rs"]]]\
-]'));
+var srcIndex = new Map(JSON.parse('[["primerlab",["",[["exercises",[],["fixme1.rs","mod.rs"]]],["functions.rs","lib.rs"]]]]'));
 createSrcSidebar();
+//{"start":36,"fragment_lengths":[86]}
