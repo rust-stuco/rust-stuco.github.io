c1a75a16e50aafc878a26b7e1de24328644a7807
Connor Tsui <connor.tsui20@gmail.com>
2025-01-28 19:42:28 -0500

    add new homework 3 (cardlab)

---
+++ hw/cardlab/doc/trait.impl/core/marker/trait.Unpin.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["cardlab",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/marker/trait.Unpin.html\" title=\"trait core::marker::Unpin\">Unpin</a> for <a class=\"struct\" href=\"cardlab/struct.Card.html\" title=\"struct cardlab::Card\">Card</a>",1,["cardlab::card::Card"]]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[282]}
