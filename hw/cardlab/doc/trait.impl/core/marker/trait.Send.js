6dc636eae7b42f1d7fc10aa331747551a8662a29
Connor Tsui <connor.tsui20@gmail.com>
2025-01-31 13:32:02 -0500

    update cardlab writeup

--- hw/cardlab/doc/trait.impl/core/marker/trait.Send.js
+++ hw/cardlab/doc/trait.impl/core/marker/trait.Send.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["cardlab",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"struct\" href=\"cardlab/struct.Card.html\" title=\"struct cardlab::Card\">Card</a>",1,["cardlab::card::Card"]]]]]);
+    var implementors = Object.fromEntries([["cardlab",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"struct\" href=\"cardlab/struct.Card.html\" title=\"struct cardlab::Card\">Card</a>",1,["cardlab::card::Card"]]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
