9635fcb849dca979bf4047e727178707cad5a7bc
connortsui20 <87130162+connortsui20@users.noreply.github.com>
2025-01-31 18:32:38 +0000

    Build: (6dc636e) update cardlab writeup

--- hw/cardlab/doc/trait.impl/core/cmp/trait.PartialOrd.js
+++ hw/cardlab/doc/trait.impl/core/cmp/trait.PartialOrd.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["cardlab",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"cardlab/struct.Card.html\" title=\"struct cardlab::Card\">Card</a>"]]]]);
+    var implementors = Object.fromEntries([["cardlab",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"cardlab/struct.Card.html\" title=\"struct cardlab::Card\">Card</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
