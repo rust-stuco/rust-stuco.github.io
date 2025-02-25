0d3a8aa0b168f6c2abb0226b713796d201a20a0f
connortsui20 <87130162+connortsui20@users.noreply.github.com>
2025-02-25 03:09:05 +0000

    Build: (b6b4ef7) update greplab writeup

--- hw/greplab/doc/src-files.js
+++ hw/greplab/doc/src-files.js
@@ -1,4 +1,3 @@
-var srcIndex = new Map(JSON.parse('[\
-["greplab",["",[],["lib.rs"]]]\
-]'));
+var srcIndex = new Map(JSON.parse('[["greplab",["",[],["lib.rs"]]]]'));
 createSrcSidebar();
+//{"start":36,"fragment_lengths":[30]}
