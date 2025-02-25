b6b4ef7253d1c0522fc6125a100829b0b0687272
Connor Tsui <connor.tsui20@gmail.com>
2025-02-24 22:08:29 -0500

    update greplab writeup

--- hw/greplab/doc/search-index.js
+++ hw/greplab/doc/search-index.js
@@ -1,5 +1,4 @@
-var searchIndex = new Map(JSON.parse('[\
-["greplab",{"doc":"98-008: Intro to the Rust Programming Language","t":"","n":[],"q":[],"d":[],"i":[],"f":[],"c":[],"p":[],"b":[]}]\
-]'));
+var searchIndex = new Map(JSON.parse('[["greplab",{"t":"","n":[],"q":[],"i":"","f":"","D":"b","p":[],"r":[],"b":[],"c":"OjAAAAAAAAA=","e":"OjAAAAAAAAA=","P":[]}]]'));
 if (typeof exports !== 'undefined') exports.searchIndex = searchIndex;
 else if (window.initSearch) window.initSearch(searchIndex);
+//{"start":39,"fragment_lengths":[122]}
