206bac3d495c74b693c215f0e491097df04dadeb
Connor Tsui <connor.tsui20@gmail.com>
2025-04-09 15:57:58 -0400

    add rowlab handout

---
+++ hw/rowlab/doc/src-files.js
@@ -0,0 +1,3 @@
+var srcIndex = new Map(JSON.parse('[["rowlab",["",[],["aggregation.rs","lib.rs","measurements.rs"]]]]'));
+createSrcSidebar();
+//{"start":36,"fragment_lengths":[64]}
---
+++ hw/rowlab/doc/trait.impl/core/panic/unwind_safe/trait.UnwindSafe.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["rowlab",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/panic/unwind_safe/trait.UnwindSafe.html\" title=\"trait core::panic::unwind_safe::UnwindSafe\">UnwindSafe</a> for <a class=\"struct\" href=\"rowlab/struct.WeatherStations.html\" title=\"struct rowlab::WeatherStations\">WeatherStations</a>",1,["rowlab::measurements::WeatherStations"]]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[368]}
