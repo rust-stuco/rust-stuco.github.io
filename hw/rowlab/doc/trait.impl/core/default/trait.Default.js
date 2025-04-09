3284c8d8cd6eca5bc5780cd480be4c31f81dfeae
connortsui20 <87130162+connortsui20@users.noreply.github.com>
2025-04-09 19:58:31 +0000

    Build: (206bac3) add rowlab handout

---
+++ hw/rowlab/doc/trait.impl/core/default/trait.Default.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["rowlab",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"rowlab/struct.WeatherStations.html\" title=\"struct rowlab::WeatherStations\">WeatherStations</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[294]}
