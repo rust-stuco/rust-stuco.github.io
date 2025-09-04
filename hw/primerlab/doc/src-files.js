b61cce3ddfe820e5f5475dc96696a0c08713c93d
YHTerrance <terranceyuhao@gmail.com>
2025-09-04 13:21:45 +0000

    Build: (ca22337) Update getownedlab and primerlab

--- hw/primerlab/doc/src-files.js
+++ hw/primerlab/doc/src-files.js
@@ -1,3 +1,2 @@
-var srcIndex = new Map(JSON.parse('[["primerlab",["",[["exercises",[],["fixme1.rs","mod.rs"]]],["functions.rs","lib.rs"]]]]'));
-createSrcSidebar();
-//{"start":36,"fragment_lengths":[86]}
+createSrcSidebar('[["primerlab",["",[["exercises",[],["fixme1.rs","mod.rs"]]],["functions.rs","lib.rs"]]]]');
+//{"start":19,"fragment_lengths":[86]}
