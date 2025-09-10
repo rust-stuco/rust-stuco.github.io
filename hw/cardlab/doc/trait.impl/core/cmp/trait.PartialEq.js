3d34c38eb4a9a231938fca455a778d2973b29661
Terrance Chen <terranceyuhao@gmail.com>
2025-09-09 22:21:02 -0400

    Update cardlab

--- hw/cardlab/doc/trait.impl/core/cmp/trait.PartialEq.js
+++ hw/cardlab/doc/trait.impl/core/cmp/trait.PartialEq.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["cardlab",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"struct\" href=\"cardlab/struct.Card.html\" title=\"struct cardlab::Card\">Card</a>"]]]]);
+    var implementors = Object.fromEntries([["cardlab",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"struct\" href=\"cardlab/struct.Card.html\" title=\"struct cardlab::Card\">Card</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
