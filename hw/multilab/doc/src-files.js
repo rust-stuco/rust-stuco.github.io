9a635fb161a299c0bd6cc45083477e16e102285f
Terrance Chen <terranceyuhao@gmail.com>
2025-09-17 09:42:36 -0400

    Update multilab

--- hw/multilab/doc/src-files.js
+++ hw/multilab/doc/src-files.js
@@ -1,3 +1,2 @@
-var srcIndex = new Map(JSON.parse('[["multilab",["",[],["lib.rs","multimap.rs","multiset.rs"]]]]'));
-createSrcSidebar();
-//{"start":36,"fragment_lengths":[59]}
+createSrcSidebar('[["multilab",["",[],["lib.rs","multimap.rs","multiset.rs"]]]]');
+//{"start":19,"fragment_lengths":[59]}
---
+++ hw/multilab/doc/trait.impl/core/marker/trait.UnsafeUnpin.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["multilab",[["impl&lt;K&gt; UnsafeUnpin for <a class=\"struct\" href=\"multilab/multiset/struct.MultiSet.html\" title=\"struct multilab::multiset::MultiSet\">MultiSet</a>&lt;K&gt;",1,["multilab::multiset::MultiSet"]],["impl&lt;K, V&gt; UnsafeUnpin for <a class=\"struct\" href=\"multilab/multimap/struct.MultiMap.html\" title=\"struct multilab::multimap::MultiMap\">MultiMap</a>&lt;K, V&gt;",1,["multilab::multimap::MultiMap"]]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[430]}
