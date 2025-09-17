337a859df1b09ffb30d9c1419b624aeedaf592f7
Connor Tsui <connor.tsui20@gmail.com>
2025-09-17 18:14:18 -0400

    update multilab handout
    Signed-off-by: Connor Tsui <connor.tsui20@gmail.com>

--- hw/multilab/doc/trait.impl/core/marker/trait.Freeze.js
+++ hw/multilab/doc/trait.impl/core/marker/trait.Freeze.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["multilab",[["impl&lt;K&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/marker/trait.Freeze.html\" title=\"trait core::marker::Freeze\">Freeze</a> for <a class=\"struct\" href=\"multilab/multiset/struct.MultiSet.html\" title=\"struct multilab::multiset::MultiSet\">MultiSet</a>&lt;K&gt;",1,["multilab::multiset::MultiSet"]],["impl&lt;K, V&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/marker/trait.Freeze.html\" title=\"trait core::marker::Freeze\">Freeze</a> for <a class=\"struct\" href=\"multilab/multimap/struct.MultiMap.html\" title=\"struct multilab::multimap::MultiMap\">MultiMap</a>&lt;K, V&gt;",1,["multilab::multimap::MultiMap"]]]]]);
+    var implementors = Object.fromEntries([["multilab",[["impl&lt;K&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/beta/core/marker/trait.Freeze.html\" title=\"trait core::marker::Freeze\">Freeze</a> for <a class=\"struct\" href=\"multilab/multiset/struct.MultiSet.html\" title=\"struct multilab::multiset::MultiSet\">MultiSet</a>&lt;K&gt;",1,["multilab::multiset::MultiSet"]],["impl&lt;K, V&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/beta/core/marker/trait.Freeze.html\" title=\"trait core::marker::Freeze\">Freeze</a> for <a class=\"struct\" href=\"multilab/multimap/struct.MultiMap.html\" title=\"struct multilab::multimap::MultiMap\">MultiMap</a>&lt;K, V&gt;",1,["multilab::multimap::MultiMap"]]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[684]}
+//{"start":57,"fragment_lengths":[680]}
